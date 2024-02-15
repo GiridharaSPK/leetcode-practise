24dd48dff01087ef2f74d56dfe25c1501e9eb45b
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-02-15 15:49:01 -0500

    Time: 29 ms (69.59%) | Memory: 63.8 MB (18.72%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.java
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.java
@@ -1,49 +1,27 @@
 class Solution {
     public long largestPerimeter(int[] nums) {
-        if(nums.length < 3 || getCount(nums) < 3) return -1;
-    
-        if(getSum(nums) <= 2 * getMax(nums)){
-            removeMax(nums);
-            return largestPerimeter(nums); 
-        }
-        return getSum(nums);
-    }
-    
-    private int getCount(int[] ar){
-        int c = 0;
-        for(int i : ar){
-            if(i > 0) c++;
-        }
-        return c;
-    }
-    
-    private long getSum(int[] ar){
-        long sum = 0;
-        for(int i : ar){
-            sum+= i;
-        } 
-        return sum;
-    }
-
-    private int getMax(int[] ar){
-        int max = -1;
-        for(int i : ar){
-            if(i > max) max=i;
-        } 
-        return max;
-    }
-
-    private void removeMax(int[] nums){
-        int m = 0;
-        int max = nums[0];
+        Arrays.sort(nums);
+        // if(nums.length < 3 || getCount(nums) < 3) return -1;
+        long[] ps = new long[nums.length];
+        ps[0] = 0;
         for(int i = 1; i < nums.length; i++){
-            if(max < nums[i]){
-                max = nums[i];
-                m = i;
+            ps[i] = ps[i-1]+nums[i-1];
+        }
+        int j = nums.length - 1;
+        while(j>0 && nums[j] >= ps[j]){
+            j--;
+        }
+        if(j < 2) return -1;
+        return ps[j] + nums[j];
+        /**while(j >= 2){
+            if(nums[j] >= ps[j]){
+                j--;
+            }else{
+                return ps[j];
             }
         }
-        nums[m] = 0;
-    }
+        return -1; */
 
+    }
 
 }
