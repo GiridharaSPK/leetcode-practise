1f88c93c50984d220eebf0ffbd2bbb9985e64590
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-02-15 23:20:25 -0500

    Time: 0 ms (100.00%) | Memory: 44.2 MB (79.68%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -4,9 +4,7 @@ class Solution {
         int f = 2;
         int s = 2;
         while(f < nums.length){
-            if(nums[f] == nums[s-2]){
-                
-            }else{
+            if(nums[f] != nums[s-2]){
                 nums[s] = nums[f];
                 s++;
             }
