c108abfc28a64487a1d37c2fee891bf7879c5f9d
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-04-02 15:04:30 -0400

    Time: 3 ms (90.04%) | Memory: 44 MB (35.59%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -3,22 +3,20 @@ class Solution {
         Stack<Integer> s = new Stack();
         int[] ans = new int[nums1.length];
         int[] temp = new int[nums2.length];
-        // HashSet<
+        
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = nums2.length-1; i>=0; i--){
             map.put(nums2[i],i);
+            
+            while(!s.isEmpty() && s.peek() < nums2[i]){
+                s.pop();
+            }
             if(s.isEmpty()){
                 temp[i] = -1;
             }else{
-                while(!s.isEmpty() && s.peek() < nums2[i]){
-                    s.pop();
-                }
-                if(s.isEmpty()){
-                    temp[i] = -1;
-                }else{
-                    temp[i] = s.peek();
-                }
+                temp[i] = s.peek();
             }
+            
             s.push(nums2[i]);
             
         }
