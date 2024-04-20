b704b4c9003b7fc670652792c54e64a1d0ef1a12
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-04-20 13:53:23 -0400

    Time: 2 ms (78.95%) | Memory: 41.7 MB (90.79%) - LeetSync

--- 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.java
+++ 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.java
@@ -4,7 +4,7 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         
         for(int d = 1; d < 10; d++) { // adding from 0 may cause stackoverflow
-            dfs(n, k, d, set);
+            dfs(n-1, k, d, set);
         }
         
         int[] ans = new int[set.size()];
@@ -17,14 +17,14 @@ class Solution {
     }
     
     private void dfs(int n, int k, int num, HashSet<Integer> set){
-        if(num >= Math.pow(10,n-1) && num < Math.pow(10,(n))) {
+        if(n == 0) {
             set.add(num);
             return;
         }
         int d = num % 10;
-        if(d + k <= 9) dfs(n, k, num*10+(d+k), set); // adding 0 to init list could cause stackoverflow
+        if(d + k <= 9) dfs(n-1, k, num*10+(d+k), set); // adding 0 to init list could cause stackoverflow
         // as d+k and further nums keep becoming 0 
-        if(k!=0 && d - k >= 0) dfs(n, k, num*10+(d-k), set);
+        if(k!=0 && d - k >= 0) dfs(n-1, k, num*10+(d-k), set);
     }
     
 }
