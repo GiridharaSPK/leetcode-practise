93e3f4b8cc0a74c16d77f8e0f457feba3b5af6d4
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-04-20 19:20:21 -0400

    Time: 3 ms (60.94%), Space: 42.3 MB (40.85%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] numsSameConsecDiff(int n, int k) {
         // dfs
-        HashSet<Integer> set = new HashSet<>();
+        /*HashSet<Integer> set = new HashSet<>();
 
         for(int d = 1; d < 10; d++) { // adding from 0 may cause stackoverflow
             dfs(n-1, k, d, set);
@@ -13,15 +13,14 @@ class Solution {
             ans[j] = i;
             j++;
         }
-        return ans;
+        return ans;*/
         
-        /* 
+        
         //bfs
         Queue<Integer> q = new LinkedList<Integer>();
         
         for(int d=1;d<10;d++){
             //adding from 0 may cause stackoverflow, anyway we dont start with 0
-            // dfs(n-1, k, d, set);
             q.add(d); 
         }
         bfs(n, k, q);
@@ -31,21 +30,39 @@ class Solution {
             ans[j] = i;
             j++;
         }
-        return ans;*/
+        return ans;
     }
     
     private void bfs(int n, int k, Queue<Integer> q){
+        int level = 1;
+        int size = q.size();
+        while(!q.isEmpty() && level < n){
+            level++;
+            size = q.size();
+            for(int i = 0; i < size; i++){
+                int num = q.remove();
+                int d = num%10;
+                if(d + k <= 9) 
+                    q.add(num*10 + d+k);
+                if(k!=0 && d - k >= 0) 
+                    q.add(num*10 + d-k);
+            }
+        }
+    }
+    
+    /*private void bfs(int n, int k, Queue<Integer> q){
         while(!q.isEmpty()){
             int num = q.peek();
-                if(num >= Math.pow(10,n-1) && num < Math.pow(10,n)) return;
+                if(num >= Math.pow(10, n-1) && num < Math.pow(10,n)) return;
             q.remove();
+            
             int d = num%10;
             if(d + k <= 9) 
                 q.add(num*10 + d+k);
             if(k!=0 && d - k >= 0) 
                 q.add(num*10 + d-k);
         }
-    }
+    }*/
     
     private void dfs(int n, int k, int num, HashSet<Integer> set){
         if(n == 0) {
