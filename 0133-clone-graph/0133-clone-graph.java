e81cb86ad365bdfa263de9910b8ddd8830741e8a
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-04-20 22:45:39 -0400

    Time: 24 ms (93.15%), Space: 42.5 MB (79.38%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        HashMap<Node, Node> map = new HashMap();
+        return clone(node, map);
+    }
+    
+    private Node clone(Node node, HashMap<Node, Node> map){
+        if(node == null) return node;
+        Node newNode = new Node(node.val);
+        map.put(node, newNode);
+        for(Node n: node.neighbors){
+            if(map.containsKey(n)){
+                newNode.neighbors.add(map.get(n));
+            }else{
+                newNode.neighbors.add(clone(n, map));
+            }
+        }
+        return newNode;
+    }
+    
+}
