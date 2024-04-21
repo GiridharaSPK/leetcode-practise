4f60d66e54417d4608ee82360c96b7107ce99872
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-04-20 22:53:11 -0400

    Time: 29 ms (10.97%), Space: 42.6 MB (48.19%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,12 +20,12 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
+        if(node == null) return null;
         HashMap<Node, Node> map = new HashMap();
         return clone(node, map);
     }
     
     private Node clone(Node node, HashMap<Node, Node> map){
-        if(node == null) return node;
         Node newNode = new Node(node.val);
         map.put(node, newNode);
         for(Node n: node.neighbors){
