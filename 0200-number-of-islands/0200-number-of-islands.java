78d13beebe52a8835c0ecf7acded2d80cf381f0d
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-02 12:21:20 -0400

    Time: 7 ms (13.93%), Space: 52.9 MB (5.59%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -6,14 +6,49 @@ class Solution {
         boolean[][] visited = new boolean[m][n];
         
         for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    islandsCount++;
+                    Queue<Point> q = new LinkedList<Point>(); 
+                    q.add(new Point(i, j));
+                    bfs(grid, visited, m, n, q);
+                }
+            }
+        }
+        
+        /*for(int i = 0; i < m; i++){
             for(int j = 0; j < n ; j++){
                 if(!visited[i][j] && grid[i][j] == '1'){
                     islandsCount++;
                     dfs(grid, visited, m, n, i, j);
                 }
             }
-        }
+        }*/
         return islandsCount;
+    }
+    
+    private void bfs(char[][] grid, boolean[][] visited,
+                     int m, int n, Queue<Point> q){
+        while(!q.isEmpty()){
+        
+            Point curr = q.remove();
+            if(!visited[curr.x][curr.y]){
+                visited[curr.x][curr.y] = true;
+                if(curr.x -1 >= 0 && grid[curr.x-1][curr.y] == '1'){
+                    q.add(new Point(curr.x-1, curr.y));
+                }
+                if(curr.x +1 < m && grid[curr.x+1][curr.y] == '1'){
+                    q.add(new Point(curr.x+1, curr.y));
+                }
+                if(curr.y-1 >= 0 && grid[curr.x][curr.y-1] == '1'){
+                    q.add(new Point(curr.x, curr.y-1));
+                }
+                if(curr.y+1 < n && grid[curr.x][curr.y+1] == '1'){
+                    q.add(new Point(curr.x, curr.y+1));
+                }
+            }
+        }
+        return;
     }
     
     private void dfs(char[][] grid, boolean[][] visited, 
@@ -34,5 +69,15 @@ class Solution {
             dfs(grid, visited, m, n, cr, cc+1);
         }
         return;
+    }
+}
+
+class Point{
+    int x;
+    int y;
+    
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
