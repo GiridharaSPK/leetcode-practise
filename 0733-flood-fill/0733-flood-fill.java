931f29e71b82b22c9c6b09752e6aba66b8ac9c91
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-04 11:04:16 -0400

    Time: 0 ms (100.00%), Space: 45.3 MB (8.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int fc = image[sr][sc];
+        int fc = image[sr][sc]; //first color
         int m = image.length;
         int n = image[0].length;
         
@@ -10,14 +10,27 @@ class Solution {
         return image;
     }
     
+    private boolean isSafe(int r, int c, int m, int n){
+        return r >=0 && r < m && c >= 0 && c < n;
+    }
+    
     private void dfs(int[][] image, int r, int c, int m, int n, int color, int fc){
-        if(image[r][c] != fc) return;
+        if(image[r][c] != fc) return; // adding extra check if  '== color' (|| image[r][c] == color)
         
         image[r][c] = color;
-        if(r+1 < m) dfs(image, r+1, c, m, n, color, fc);
+        int[][] dir = {{-1,0}, {0, 1}, {1, 0}, {0, -1}};
+        for(int k=0; k < 4; k++){
+            int nr = r+dir[k][0];
+            int nc = c+dir[k][1];
+            if(isSafe(nr, nc, m, n)){
+                dfs(image, nr, nc, m, n, color, fc);
+            }
+        }
+        
+        /*if(r+1 < m) dfs(image, r+1, c, m, n, color, fc);
         if(c+1 < n) dfs(image, r, c+1, m, n, color, fc);
         if(c-1 >= 0) dfs(image, r, c-1, m, n, color, fc);
-        if(r-1 >= 0) dfs(image, r-1, c, m, n, color, fc);
+        if(r-1 >= 0) dfs(image, r-1, c, m, n, color, fc);*/
 
     }
 }
