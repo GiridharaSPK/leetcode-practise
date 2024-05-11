fc6225ffc2203b98e53f2b80746c7ea080b0eecb
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-10 21:02:27 -0400

    Time: 6 ms (48.29%), Space: 46.1 MB (6.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         HashMap<Integer, ArrayList<Integer>> graph = new HashMap();
         HashSet<Integer> visited = new HashSet();
+        HashSet<Integer> done = new HashSet();
         for(int i = 0; i < prerequisites.length; i++){
             graph.computeIfAbsent(
                 prerequisites[i][0],  
@@ -10,27 +11,28 @@ class Solution {
         }
         
         for(int i = 0; i < numCourses; i++){       
-            boolean check = dfs(graph, i, visited);
+            boolean check = dfs(graph, i, visited, done);
             if(!check) return false;
         }
         return true;
     }
     
-    private boolean dfs(HashMap<Integer, ArrayList<Integer>> graph, int i, HashSet<Integer> visited){
+    private boolean dfs(HashMap<Integer, ArrayList<Integer>> graph, int i, 
+                        HashSet<Integer> visited, HashSet<Integer> done){
         if(visited.contains(i)){
            return false; 
         }
-        if((graph.get(i) == null)){
+        if(done.contains(i) || (graph.get(i) == null)){
             return true;
         }
+        done.add(i);
         visited.add(i);
         ArrayList<Integer> adj = graph.get(i);
         for(int j = 0; j < adj.size(); j++){
-            boolean check = dfs(graph, adj.get(j), visited);
+            boolean check = dfs(graph, adj.get(j), visited, done);
             if(!check) return false; 
         }
         visited.remove(i);
-        graph.put(i, null);
         return true;
     }
 }
