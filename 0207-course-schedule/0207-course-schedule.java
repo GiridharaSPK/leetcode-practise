5465d1a2bf4e41f53d055ef3808915294e1dd30d
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-10 20:54:13 -0400

    Time: 5 ms (67.75%), Space: 45.8 MB (19.57%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, ArrayList<Integer>> graph = new HashMap();
+        HashSet<Integer> visited = new HashSet();
+        for(int i = 0; i < prerequisites.length; i++){
+            graph.computeIfAbsent(
+                prerequisites[i][0],  
+                v -> new ArrayList()
+            ).add(prerequisites[i][1]);
+        }
+        
+        for(int i = 0; i < numCourses; i++){       
+            boolean check = dfs(graph, i, visited);
+            if(!check) return false;
+        }
+        return true;
+    }
+    
+    private boolean dfs(HashMap<Integer, ArrayList<Integer>> graph, int i, HashSet<Integer> visited){
+        if(visited.contains(i)){
+           return false; 
+        }
+        if((graph.get(i) == null)){
+            return true;
+        }
+        visited.add(i);
+        ArrayList<Integer> adj = graph.get(i);
+        for(int j = 0; j < adj.size(); j++){
+            boolean check = dfs(graph, adj.get(j), visited);
+            if(!check) return false; 
+        }
+        visited.remove(i);
+        graph.put(i, null);
+        return true;
+    }
+}
