8e90f6e1c6dbfaf1c17d3779f4e27c00dd0f477b
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-18 16:27:36 -0400

    Time: 0 ms (100.00%), Space: 44.1 MB (67.72%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,9 +15,33 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        ArrayList<Integer> list = inorder(root, new ArrayList(), k-1);
-        if(list.size() > k-1) return list.get(k-1);
+        
+        //iterative approach  ( = stack + logic of recursive)
+        int count = 0;
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        TreeNode node = root;
+        while(!s.isEmpty() || node != null){ // inorder traversal
+            //left
+            while(node!=null){
+                s.push(node);
+                node = node.left;
+            }
+            
+            //current
+            node = s.pop();
+            count++;
+            if(count == k) return node.val;
+                
+            //right
+            node = node.right;
+        }
+        
         return -1;
+        
+        //recursive approach
+        /*ArrayList<Integer> list = inorder(root, new ArrayList(), k-1);
+        if(list.size() > k-1) return list.get(k-1);
+        return -1;*/
     }
     
     private ArrayList<Integer> inorder(TreeNode node, ArrayList<Integer> list, int k){
