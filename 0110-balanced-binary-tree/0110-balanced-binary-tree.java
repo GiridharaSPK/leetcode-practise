f6d7a491412d03267fa83380310f7cdb3cdef28d
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-19 22:37:33 -0400

    Time: 0 ms (100.00%), Space: 44 MB (78.89%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,15 +16,32 @@
 class Solution {
     //height balanced : difference in left and right subtree heights is <= 1 for each node
     public boolean isBalanced(TreeNode root) {
+        return helper(root) != -2;
+        
+        // O(n^^2) approach - top down approach
         // test case - empty tree
-        if(root == null) return true;
+        /*if(root == null) return true;
         
         if(Math.abs(height(root.left) - height(root.right)) < 2){
             return isBalanced(root.left) && isBalanced(root.right);
         }
-        return false;
+        return false;*/
     }
     
+    private int helper(TreeNode node){ // returns height and isbalanced - returns height or -2
+        if(node == null) return -1;
+        
+        int left = helper(node.left);
+        int right = helper(node.right);
+        if(left == -2 || right == -2) return -2;
+        
+        if(Math.abs(left - right) < 2) 
+            return Math.max(left, right)+1;
+        
+        return -2;
+    }
+    
+    // O(n) for each node
     private int height(TreeNode node){
         if(node == null) return -1;
         
