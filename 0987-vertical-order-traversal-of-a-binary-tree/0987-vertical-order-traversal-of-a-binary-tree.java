e657183ee8821cfdf54897e15015cc20ea85073f
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-21 08:45:34 -0400

    Time: 2 ms (99.40%), Space: 42.2 MB (71.31%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        int[] minMax = {0,0};
+        HashMap<Integer, PriorityQueue<int[]>> map = new HashMap<Integer, PriorityQueue<int[]>>();
+        helper(root, 0, 0, map, minMax);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = minMax[0]; i <= minMax[1]; i++){
+            PriorityQueue<int[]> pq = map.get(i);
+            List<Integer> list = new ArrayList();
+            while(!pq.isEmpty()){
+                int[] ar = pq.poll();
+                list.add(ar[2]);
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+    
+    private void helper(TreeNode root, int pos, int level,
+                        HashMap<Integer, PriorityQueue<int[]>> map, int[] minMax){
+        if(root == null) return;
+        
+        if(map.containsKey(pos)){
+            PriorityQueue<int[]> pq = map.get(pos);
+            int[] ar = {level, pos, root.val};
+            pq.offer(ar);            
+        }else{
+            PriorityQueue<int[]> pq = 
+                new PriorityQueue<int[]>((a,b)->(
+                    (a[0]-b[0]!=0 ? a[0]-b[0] : a[2]-b[2])
+                ));
+            int[] ar = {level, pos, root.val};
+            pq.offer(ar);
+            map.put(pos, pq);
+        }
+        
+        if(pos <= minMax[0]) minMax[0] = pos;
+        if(pos >= minMax[1]) minMax[1] = pos;
+
+        helper(root.left, pos-1, level+1, map, minMax);
+        helper(root.right, pos+1, level+1, map, minMax);
+    }
+}
