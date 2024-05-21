290f1141767a72f6316f6df1be2c7e2c3b57452d
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-21 17:21:49 -0400

    Time: 5 ms (17.17%), Space: 41.6 MB (30.36%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,14 +1,76 @@
 class Solution {
     public String decodeString(String s) {
-        // test cases - nested brackets[]
-        // test cases - always number
-        // every substring has a number? i.e. 1[ef] or just ef
+        // test cases or questions or scenarios to think of
+        // 1 - nested brackets[]
+        // 2 - always a number exists
+        // 3 - does every substring has a number? i.e. 1[ef] or just ef
+        // 4 - tailing characters - dont forget chars not part of stack in the end
+        // 5 - does it always start with num and end with ]?
+        
+        // approach1: using 2 stacks - counts and strings
+        
+        Stack<Integer> counts = new Stack<Integer>();
+        Stack<String> strings = new Stack<String>();        
+        String strNum = ""; // String(num) - variable to perform logic 
+        
+        // normal chars keep append to string 
+        String str = ""; // stores current string
+        
+        // at digit keep appending strNum (add to stack at '[' )
+        
+        // at [ (have to multiply) and (possibility of nesting)
+        // so, add existing string to string stack - to pop after resolving current str
+        // and add count to count stack
+        // reset str (and strNum)
+        
+        // between [ and ] and not digit
+        // keep appending str (store currently traversing string - has freq top of counts)
+        
+        // at ] pop the count and resolve current str 
+        // after resolving append to actual string which is on top of strings stack
+        // 
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == ']'){
+            // closing current string - resolve and append with top of stack
+
+                // pop count 
+                int num = counts.pop();
+
+                // resolve str num times
+                String temp = "";
+                while(num>0){ 
+                    num--;
+                    temp += str;
+                }
+                str = strings.pop() + temp;
+            }else if(Character.isDigit(ch)){
+                // keep adding to strNum (add to count stack at '[' )
+                strNum += String.valueOf(ch);
+            }else if(ch == '['){
+                // adding count of current string to count stack
+                counts.push(Integer.parseInt(strNum));
+                // adding prev str to strings, 
+                // (will pop it and append with current string is done at ])
+                strings.push(str);
+                strNum = "";
+                str = "";
+            }else{
+                // normal chars keep append to string 
+                str = str + ch;
+            }
+        }
+        
+        return str;
+        
+        // approach2: using a single stack
         
         // Use Stack to push all not just numbers,
         // use same stack for all chars 
         // and pop at ] until [ and number and repeat;
         
-        Stack<String> stack = new Stack();
+        /*Stack<String> stack = new Stack<String>();
         int num = 0;
         
         for(int i = 0; i < s.length(); i++){
@@ -51,7 +113,7 @@ class Solution {
         while(!stack.isEmpty()){
            a = stack.pop()+a;
         }
-        return a;
+        return a;*/
         
     }
 }
