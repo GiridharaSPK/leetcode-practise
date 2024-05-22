9558918e5724730aee93313227150422e7a3b4be
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-22 16:02:38 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (90.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,16 +1,46 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        helper(0, matrix.length-1, matrix);
+        
+        // approach2: transpose and swap
+        convertRowsToCols(matrix);
+        swapCols(matrix); //0,n .. 1,n-1 .. 
+        
+        // approach1: rotate n-1 times each edge recursively
+        // swapHelper(0, matrix.length-1, matrix);
     }
     
-    private void helper(int start, int end, int[][] matrix){
+    private void convertRowsToCols(int[][] matrix){
+        for(int r = 0; r < matrix.length; r++){
+            for(int c = r+1; c < matrix[0].length; c++){
+                swap(matrix, r, c, c, r);
+            }
+        }
+    }
+    
+    private void swap(int[][] matrix, int r1, int c1, int r2, int c2){
+        int temp = matrix[r1][c1];
+        matrix[r1][c1] = matrix[r2][c2];
+        matrix[r2][c2] = temp;
+    }
+    
+    private void swapCols(int[][] matrix){
+        int n = matrix.length;
+        for(int r = 0; r < n; r++){
+            for(int c = n/2; c < n; c++){
+                swap(matrix, r, c, r, n-c-1);
+            }
+        }
+    }
+
+    // approach 1
+    private void swapHelper(int start, int end, int[][] matrix){
         if(start > end) return;
         int n = end - start;
         while(n > 0){
             n--;
             swapOnce(start, end, matrix);
         }
-        helper(start+1, end-1, matrix);
+        swapHelper(start+1, end-1, matrix);
     }
     
     private void swapOnce(int start, int end, int[][] matrix){
