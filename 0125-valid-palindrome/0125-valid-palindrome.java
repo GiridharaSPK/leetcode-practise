753d4bb1625c1abdc2b0c2c03a5f87a78ef357b2
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-25 19:16:02 -0400

    Time: 3 ms (73.71%), Space: 42.5 MB (91.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,33 +3,35 @@ class Solution {
         // test cases
         //1 - only alphabets? should i ignore others
         //2 - empty string
+        //3 - all non alphanumeric chars
         if(s.isEmpty()) return true;
         
         int start = 0;
         int last = s.length()-1;
         
         // approch 1: with single while loop
-        // doing only 1 thing in an iteration - (slower than approach 1)
-            // either checking start and last ptrs if not valid and move ptrs to valid chars
-            // or comparing chars for palindrome check and ** move to next
-        /**while(last >= start){
-            if(last < 0) break;
-            if(start >= s.length()) break;
-            if(!Character.isDigit(s.charAt(start)) && !Character.isLetter(s.charAt(start))){
+        // doing only 1 thing in an iteration
+            // either checking start and last ptrs if not valid and move ptrs to next chars
+            // or comparing current chars for palindrome and move to next chars
+            // in both cases moving only 1 step in 1 iteration
+        while(last >= 0 && start < s.length() && last >= start){
+            char cs = s.charAt(start);
+            char cl = s.charAt(last);
+            
+            if(!Character.isLetterOrDigit(cs)){
                 start++;
-            }else if(!Character.isDigit(s.charAt(last)) && !Character.isLetter(s.charAt(last))){
+            }else if(!Character.isLetterOrDigit(cl)){
                 last--;
             }else{
-                if(Character.toLowerCase(s.charAt(start)) 
-                   != Character.toLowerCase(s.charAt(last))) 
+                if(Character.toLowerCase(cs) != Character.toLowerCase(cl)) 
                     return false;
                 start++;
                 last--;
             }
-        }*/
+        }
         
         // approach 1 : with multiple while loops
-        while(last >= start){
+        /**while(last >= start){
             // should be 'while' not 'if' - move to next character
             while(last >= 0){  //** cannot combine isLetter check and decrement 
                 // can decrease to -ve inside this loop 
@@ -50,7 +52,7 @@ class Solution {
                 return false;
             start++;
             last--;
-        }
+        }*/
 
         return true;
     }
