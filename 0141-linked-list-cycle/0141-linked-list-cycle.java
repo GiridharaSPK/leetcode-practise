40c362defb249214f7184484f872bf3edb27de32
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-28 18:31:45 -0400

    Time: 0 ms (100.00%), Space: 44.3 MB (56.73%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,12 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null) return false;
+        // if(head == null) return false;
         
         ListNode fast = head;
         ListNode slow = head;
         
-        while(slow.next!=null && fast.next!=null && fast.next.next!=null){
+        // while(slow.next!=null && fast.next!=null && fast.next.next!=null){
+        // checking for curr pointers instead of cur.next help avoid null head base case
+        while(slow!=null && fast!=null && fast.next!=null){
             fast = fast.next.next;
             slow = slow.next;
             
