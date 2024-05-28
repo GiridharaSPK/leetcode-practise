580f7a5195d2003bbce59aeac7eb3a69bb3903ff
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-28 14:22:42 -0400

    Time: 1 ms (99.73%), Space: 61.2 MB (87.77%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,43 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[] maxFromHere = new int[n];
+        int max = 0;
         
+        // approach 2: sliding window
+        if(n < 2) return 0;
+        
+        int left = 0;
+        int right = 1;
+        
+        while(right < n){
+            while(right < n && prices[left] < prices[right]){
+                if(max < prices[right] - prices[left]){
+                    max = prices[right] - prices[left];
+                }
+                right++;
+            }
+            left = right;
+            right = left+1;
+        }
+        
+        
+        // approach 1: precompution of best sell + traversal for max profit
+        // O(n) time + O(n) space
+        // pre computation of max at each i
+        /*int[] maxFromHere = new int[n];
         maxFromHere[n-1]= prices[n-1];
         for(int i = n-2; i >= 0; i--){
             maxFromHere[i] = Math.max(maxFromHere[i+1], prices[i]);
         }
         
-        int max = 0;
+        // traversing for max profit at each i
         for(int i= 0; i < n-1; i++){//left ptr
             int profit = maxFromHere[i] - prices[i];
             if(max < profit){
                 max = profit;
             }
-        }
+        }*/
+        
         return max;
     }
 }
