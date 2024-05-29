7bfa2cf7b25ea59c1c9ef29db367690af69c4008
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-29 14:41:44 -0400

    Time: 7 ms (45.94%), Space: 47.2 MB (93.57%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -8,11 +8,21 @@ class Solution {
                 map.put(num, map.get(num)-1);
             }else if(map.size() == 3){ 
                 // reduce count by 1 and add element
-                for(int key : map.keySet()){
+                
+                // reduce count and remove if 0
+                /*for(int key : map.keySet()){
                     map.put(key, map.get(key)-1);
                 }
                 // removing 0 valued keys to maintain hashmap size less than 3
-                map.entrySet().removeIf(entry -> entry.getValue() == 0);
+                map.entrySet().removeIf(entry -> entry.getValue() == 0);*/
+                
+                // create a new temp set to avoid concurrency exception
+                for (int k : new HashSet<Integer>(map.keySet()))
+                if (map.get(k) == 1)
+                    map.remove(k);
+                else
+                    map.put(k, map.get(k) - 1);
+                // add element
                 map.put(num, 1);
             }else{ 
                // seen new element, and map size < 3 - add to map
