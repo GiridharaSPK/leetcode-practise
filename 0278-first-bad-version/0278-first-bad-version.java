be4d011e1f573f6ca279d34a7cab512f969c3d2c
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-29 00:43:06 -0400

    Time: 25 ms (72.53%), Space: 40.1 MB (72.53%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -23,5 +23,24 @@ public class Solution extends VersionControl {
             }
         }
         return first; // always dont return mid - based on logic return first/mid/last
+        
+        // trying for more optimal approach - 100%
+        // got 72.53%
+        /*int first = 1;
+        int last = n;
+        int mid = 0;
+        int ans = n;
+        
+        while(first <= last){
+            mid = first + (last-first)/2;
+            if(isBadVersion(mid)){
+                last = mid-1;
+                ans = mid;
+            }else{
+                first = mid+1;
+            }
+        }
+        return ans; */
+    
     }
 }
