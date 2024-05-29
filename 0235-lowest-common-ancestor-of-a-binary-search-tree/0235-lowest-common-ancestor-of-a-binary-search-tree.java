b52a490e985b5ca96cd716c375f7e9a24ec71348
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-28 23:11:35 -0400

    Time: 5 ms (100.00%), Space: 44.9 MB (42.82%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -23,9 +23,11 @@ class Solution {
         }
         return root;
         
-        // recursive approach
+        // recursive approach - base cases very important
         /*if(p == q) return p;
-        if(root==null || root == p || root == q) return root;
+        if(root==null || root == p || root == q) return root; 
+        // ** root==p and root==q important base cases
+        
         if(p.val < root.val && root.val < q.val 
            || p.val > root.val && root.val > q.val){
             return root;
