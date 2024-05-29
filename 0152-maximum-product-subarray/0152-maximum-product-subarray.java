d8adf68d1bb39afc167976a8db668850ea514b77
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-29 18:28:45 -0400

    Time: 3 ms (20.77%), Space: 44.7 MB (63.22%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
-        int maxProduct = Integer.MIN_VALUE;
         
         // striver's approach - calculate product from 2 sides reset at 0
+        /*int maxProduct = Integer.MIN_VALUE;
         int pref_product = 1; // to keep track of products from left
         int suf_product = 1; //to keep track of products from right
         int i = 0;
@@ -24,6 +24,36 @@ class Solution {
             }
             i++;
         }
-        return maxProduct;
+        
+        return maxProduct;*/
+        
+        //neetcode's approach
+        // at 0s we reset the values 
+        // to track negative products we need to maintain 2 products 
+        // currMax and currMin 
+        // currMin could exceed currMax if multiplied again with another num
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int i = 0;
+        int currMax = 1;
+        int currMin = 1;
+        while(i < n){
+            // storing them so that we compare values before updating each of them
+            int temp1 = currMax*nums[i]; 
+            int temp2 = currMin*nums[i];
+            currMax = Math.max(Math.max(temp1, temp2), nums[i]); 
+            // if temp is not used currMax gets updated for next step
+            currMin = Math.min(Math.min(temp1, temp2), nums[i]);
+            max = Math.max(max, currMax);
+            min = Math.min(min, currMin);
+            if(currMax == 0)
+                currMax = 1;
+            if(currMin == 0)
+                currMin = 1;
+            i++;
+        }
+        
+        return max;
     }
 }
