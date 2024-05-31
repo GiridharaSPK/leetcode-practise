89d504c4c7f3dc7ba031f3d216ae912fd1bd1614
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-05-31 18:22:55 -0400

    Time: 2 ms (51.96%), Space: 43 MB (65.82%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,7 +1,14 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
+        
+        // recursion/ backtracking apporach 
+        // keep generating brackets but with check on num of open and close brackets
+        List<String> ans = new ArrayList<String>();
+        helper(n, "", 0, 0, ans);
+        return ans;
+        
         // dynamic programming approach - sumeet
-        List<String>[] dp = new List[n+1]; // dont use List<Stirng>[n+1] 
+        /*List<String>[] dp = new List[n+1]; // dont use List<Stirng>[n+1] 
         dp[0] = Arrays.asList("");
         dp[1] = Arrays.asList("()");
         
@@ -31,6 +38,27 @@ class Solution {
             }
         }
         
-        return dp[n];
+        return dp[n];*/
     }
+    
+    private void helper(int n, String str,
+                                 int openCount, int closedCount, 
+                                 List<String> ans){
+        
+        if(openCount == n && closedCount == n){
+            ans.add(str);
+            return;
+        }
+        
+        if(openCount < n){
+            // str = str + "(";// dont use same string variable in both recursive calls
+            helper(n, str+"(", openCount+1, closedCount, ans);
+        }
+        if(closedCount < openCount){ // count of ) should be <= count of (
+            // str = str + ")";
+            helper(n, str+")", openCount, closedCount+1, ans);
+        }
+        
+    }
+    
 }
