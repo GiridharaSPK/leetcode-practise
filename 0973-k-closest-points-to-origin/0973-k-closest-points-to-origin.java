f06f514e3228debad32a5bea1c10fdf06084b0a9
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-01 12:44:19 -0400

    Time: 809 ms (5.05%), Space: 52.9 MB (49.12%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,6 +1,25 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        // imp - edge case n=k
+        // quick sort - quick select
+        int low = 0;
+        int high = points.length-1;
+        while(low < high){
+            int pivot = partition(points, low, high); //returns pivot index 
+            // left of pivot all elements less than pivot
+            if(pivot == k){
+                break;
+            }else if(pivot < k){
+                low = pivot+1;
+            }else{
+                high = pivot-1;
+            }
+        }
+        
+        return Arrays.copyOfRange(points, 0, k);
+        
+        /* Priority Queue approach 56 ms
+        // imp - edge case when n = k
+        // edge case when pq.size() doesnt exceed k at any point
         // create a max heap
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b)-> dist(b) - dist(a));
         for(int i = 0; i < points.length; i++){
@@ -11,11 +30,10 @@ class Solution {
             if(pq.size() > k){ 
                 pq.poll();
             }
-            int[] p = {points[i][0], points[i][1]};
-            pq.add(p);
+            pq.offer(points[i]);
             // pq at any give point has a max of k+1 elements or less
         }
-        if(pq.size() == k+1) // check size before removing (k+1)th element
+        if(pq.size() == k+1) // imp - check size before removing (k+1)th element
             pq.poll();
         
         int[][] ans = new int[k][2];
@@ -26,11 +44,36 @@ class Solution {
             ans[i][1] = p[1];
             i++;
         }
-        return ans;
+        return ans;*/
+    }
+    
+    private int partition(int[][] points, int low, int high){
+        int[] pivot = points[high];
+        int ptr = low;
+        // distance from origin of ptr index point is less than pivot
+        // swap the ithe position val with ptr 
+        for(int i = low; i < high; i++){   
+            if(isFirstPointerCloser(points[i], pivot)){
+                int[] temp = points[i];
+                points[i] = points[ptr];
+                points[ptr] = temp;
+                ptr++;
+            }
+        }
+        // swap ptr and pivot i.e. high index
+        int[] temp = points[ptr];
+        points[ptr] = points[high];
+        points[high] = temp;
+        
+        return ptr;
     }
     
     private int dist(int[] p){
         return (int)(Math.pow(p[0],2)+ Math.pow(p[1],2));
+    }
+    
+    private boolean isFirstPointerCloser(int[] p1, int[] p2){
+        return dist(p1) < dist(p2);
     }
     
 }
