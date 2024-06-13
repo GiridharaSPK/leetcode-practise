e5f3352a432773544cd4a14d8a6942ce8de9e48a
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-13 19:10:30 -0400

    Time: 254 ms (65.55%), Space: 47.3 MB (10.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
@@ -1,9 +1,10 @@
 class Solution {
     fun setZeroes(matrix: Array<IntArray>): Unit {
+        // approach 1
         // doing in O(1) space 
-        //using first row and first col as flags
+        // using first row and first col as flags
         
-        var frow = false // flag to mark if the 1st row originally have 0s
+        /*var frow = false // flag to mark if the 1st row originally have 0s
         var fcol = false // flag to mark if the 1st col originally have 0s
         for(i in 0 until matrix.size){
             for(j in 0 until matrix[0].size){
@@ -13,6 +14,12 @@ class Solution {
                     matrix[i][0] = 0
                     matrix[0][j] = 0
                 }
+            }
+        }
+        
+        if(matrix[0][0] == 0){
+            for(i in 0 until matrix.size){
+                matrix[i][0] = 0
             }
         }
         
@@ -30,19 +37,51 @@ class Solution {
             }
         }
         
-        // if(matrix[0][0] == 0){
-        //     for(i in 0 until matrix.size){
-        //         matrix[i][0] = 0
-        //     }
-        // }
-        
         if(fcol){
+            for(i in 0 until matrix.size){
+                matrix[i][0] = 0
+            }
+        }*/
+        
+        // approach 2 (extension of approach 1) with single flag
+        // doing in O(1) space 
+        
+        var frow = false // flag to mark if the 1st row originally have 0s
+        for(i in 0 until matrix.size){
+            for(j in 0 until matrix[0].size){
+                if(matrix[i][j] == 0){
+                    if(i == 0) 
+                        frow = true 
+                    else // not marking matrix[0][0] = 0 for first row
+                        matrix[i][0] = 0
+                    
+                    matrix[0][j] = 0
+                }
+            }
+        }
+
+        for(i in 1 until matrix.size){
+            for(j in 1 until matrix[0].size){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0
+                }
+            }
+        }
+        
+        if(matrix[0][0] == 0){
             for(i in 0 until matrix.size){
                 matrix[i][0] = 0
             }
         }
         
-        /*
+        if(frow){
+            for(i in 0 until matrix[0].size){
+                matrix[0][i] = 0
+            }
+        }
+        
+        
+        /* O(n) space approach
         val rows = HashSet<Int>();
         val cols = HashSet<Int>();
         
