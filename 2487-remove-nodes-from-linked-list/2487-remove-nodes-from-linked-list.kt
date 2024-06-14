45c7d8aa8d7819f1fac3bd7ad46f20dacf0e6f2c
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-13 21:52:47 -0400

    Time: 679 ms (95.23%), Space: 63.4 MB (40.46%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.kt
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.kt
@@ -8,8 +8,47 @@
  * }
  */
 class Solution {
+ 
+    private fun reverse(head: ListNode) : ListNode{
+        val dummy = ListNode(0)
+        var right = head
+        var left : ListNode? = null
+        var ptr : ListNode? = null
+        while(right!=null){
+            ptr = right.next
+            right.next = left
+            left = right
+            right = ptr
+        }
+        
+        return left!!
+    }
+    
     fun removeNodes(head: ListNode?): ListNode? {
         head ?: return null
+        // better approach without extra space
+        // reverse the list 
+        // traverse and delete smaller nodes
+        val newHead = reverse(head)
+        var ptr : ListNode? = newHead
+        var max = newHead.`val`
+        var prev : ListNode? = null
+        while(ptr!=null){
+            if(max <= ptr!!.`val`){
+                max = ptr!!.`val`
+                prev = ptr
+                ptr = ptr!!.next
+            }else {
+                prev!!.next = ptr!!.next
+                ptr = ptr!!.next
+            }
+
+        }
+        return reverse(newHead)
+    
+        /* 
+        // O(n) using stack to traverse in reverse 
+        // O(n) new space to create new linkedList
         val stack = Stack<Int>()
 
         var ptr = head
@@ -31,6 +70,6 @@ class Solution {
             }
         }
         
-        return prev
+        return prev*/
     }
 }
