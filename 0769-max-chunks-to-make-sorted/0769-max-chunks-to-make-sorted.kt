0827e4be696477475110c879f42b9715480e98f6
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-13 20:57:11 -0400

    Time: 137 ms (25.00%), Space: 33.6 MB (50.00%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.kt
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.kt
@@ -1,14 +1,42 @@
 class Solution {
     fun maxChunksToSorted(arr: IntArray): Int {
+        // without O(n) space - given integers range from 0 to n-1 and unique
+        // max until now should be equal to index - to chunk
+        var max = arr[0]
+        var count = 0
+        for(i in arr.indices){
+            if(max < arr[i]) max = arr[i]
+            if(max == i) count++
+        }
+        return count
+        
+        // pre computation approach - O(n) space + O(n) time
+        /*val minArr = IntArray(arr.size) { ind -> arr[ind] }
+        // stores min from right to index
+        for(ind in arr.size-2 downTo 0){
+            minArr[ind] = Math.min(minArr[ind+1], minArr[ind])
+        }
+        var max = Int.MIN_VALUE // max until now
+        var count = 0
+        for(i in arr.indices){
+            if(arr[i] > max)
+                max = arr[i]
+            if(i+1< arr.size && max <= minArr[i+1]){ // max until now <= min from next
+                count++
+            }
+        }
+        return count+1 // (last chunk) */
+        
+        // brute force - O(n^2)
         // if sorted - break into single elements
         // if not sorted - chunk
-        var count = 0
+        /*var count = 0
 // 2, 1, 6, 8, 9        
         for(i in arr.indices){
             var min = Int.MAX_VALUE
             var max = Int.MIN_VALUE
             for(j in arr.indices){
-                // find max in left of i ()
+                // find max in left of i (including i)
                 if(max < arr[j] && j <= i){
                     max = arr[j]
                 }
@@ -21,7 +49,7 @@ class Solution {
                 count++
             }
         }
-        return count
+        return count*/
     }
 }
 
