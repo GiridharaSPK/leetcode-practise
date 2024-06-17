70e3d251866c8304bec11042f94225d91a7e6b51
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-17 00:17:56 -0400

    Time: 127 ms (88.89%), Space: 33 MB (77.78%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.kt
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.kt
@@ -1,14 +1,30 @@
 class Solution {
     fun judgeSquareSum(c: Int): Boolean {
-        // approach 1 - Linear O(n) - similar to Two Sum
-        val set = HashSet<Int>()
+        
+        // approach 2 - Two pointer
+        var l = 0
+        var r = Math.sqrt(c.toDouble()).toInt()
+        
+        while(l <= r){
+            val sum = l*l + r*r
+            if(sum == c) return true
+            
+            // added sum < 0 for sum overflow cases
+            if(sum > c || sum < 0) r--
+            else l++
+        }
+        return false
+        
+        
+        // approach 1 - Linear O(sqrt of c) - similar to Two Sum
+        /*val set = HashSet<Int>()
         
         val root = Math.sqrt(c.toDouble()).toInt();
         for(i in 0..root){
             set.add(c - i*i);
             if(set.contains(i*i)) return true;
         }
-        return false;
+        return false;*/
         
     }
 }
