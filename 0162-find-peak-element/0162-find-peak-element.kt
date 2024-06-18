01a3c8f9e1504bb466c2dac8930ae5f19ffadb0a
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-18 00:46:50 -0400

    Time: 188 ms (18.84%), Space: 37.7 MB (25.21%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.kt
+++ 0162-find-peak-element/0162-find-peak-element.kt
@@ -6,6 +6,12 @@ class Solution {
         // [1,3,2]
         // [1,2,3]
         // [3,2,1]
+        // [1 3 4 2]
+        // [1 4 3 2]
+
+        // we have to find just 1 peak
+        // so look for most probable peak from mid at every iteration
+        // i.e. look for nums[mid] and nums[mid+1] and move towards a potential solution
         while(l < r){
             val mid = l + (r-l)/2
             if(nums[mid] > nums[mid+1]){
@@ -15,5 +21,12 @@ class Solution {
             }
         }
         return l
+        
+        // Initially i chose to compare nums[mid], nums[mid-1]
+        // where i have to handle and extra case of mid == 0 
+        // and still giving TLEs for others cases
+        // ** NOTE : if we have to handle extra cases like that, 
+        // ** (hint)  we have to think of a different approach
+        // it was also ambigous in chosing to update l or r with test cases
     }
 }
