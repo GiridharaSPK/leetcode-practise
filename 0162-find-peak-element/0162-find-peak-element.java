5b434d71f992216aef1e40451789e79cdbe20e63
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-18 00:47:11 -0400

    Time: 0 ms (100.00%), Space: 42.7 MB (9.13%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -4,6 +4,7 @@ class Solution {
         int r = nums.length-1;
         // we have to find just 1 peak
         // so look for most probable peak from mid at every iteration
+        // i.e. look for nums[mid] and nums[mid+1] and move towards a potential solution
         while(l < r){
             int mid = l + (r-l)/2;
             if(mid+1 < nums.length && nums[mid] < nums[mid+1]){
@@ -13,5 +14,13 @@ class Solution {
             }
         }
         return l;
+        
+        // Initially i chose to compare nums[mid], nums[mid-1]
+        // where i have to handle and extra case of mid == 0 
+        // and still giving TLEs for others cases
+        // ** NOTE : if we have to handle extra cases like that, 
+        // ** (hint)  we have to think of a different approach
+        // it was also ambigous in chosing to update l or r with test cases
+        
     }
 }
