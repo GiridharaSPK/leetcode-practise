70f619137100c81b7f9e75aa5482b7601159c48f
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-24 19:56:20 -0400

    Time: 16 ms (13.13%), Space: 56.7 MB (45.63%) - LeetHub

---
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -0,0 +1,111 @@
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+        // approach by neetcode
+        int count = 0;
+        Deque<Integer> q = new LinkedList<Integer>(); // stores indexes of flipped
+        int i = 0;
+        while(i < nums.length){
+            // if queue has old/irrelevant indexes remove
+            while(!q.isEmpty() && q.peekFirst() < i - k + 1){
+                q.pollFirst();
+            }
+            
+            // there is 0 in the hypothetical array - flip k elements
+            if((nums[i] + q.size()) % 2 == 0){
+                if(i + k > nums.length){
+                    return -1;
+                }
+                count++;
+                q.offer(i);
+            }
+            i++;
+        }
+        return count;
+        
+        
+        // apporach without actually flipping
+        // too many edge cases
+        /*int count = 0;
+        int s = 0;
+        int bit = 0;
+        int i = 0;
+        while(i < nums.length){
+            if(nums[i] == bit){  // 3, 0000110 | 3, 000011 | 3, 0000111
+                int j = i;
+                count++;
+                while(j < i + k){
+                    if(j < nums.length && nums[j] == bit){
+                        j++;
+                    }else{
+                        bit = 1-bit;
+                        break;
+                    }
+                }
+                if(j == nums.length) 
+                    return count;
+                if(j < i + k){
+                    bit = 0;
+                }
+                i = j;
+            }else{
+                i++;
+            }
+        }
+        // check for -1
+        return count;*/
+    } 
+
+        // apporach - flipping k from every 0 and continue from first 1
+        // gives TLE for last 10 big inputs
+    /*
+    public int minKBitFlips(int[] nums, int k) {   
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i < nums.length - k + 1){
+                if(nums[i] == 0){
+                    i = flip(nums, i, k) - 1;
+                    count++;
+                }
+            }else{
+                if(nums[i] == 0) return -1;
+            }
+        }
+        return count;
+    }
+    
+    private int flip(int[] nums, int i, int k){
+        int flag = -1;
+        for(int j = i; j < i+k; j++){
+            if(nums[j] == 1 && flag == -1){
+                flag = j;
+            }
+            nums[j] = 1-nums[j];
+        }    
+        if(flag == -1)
+            return i+k;
+        return flag;
+    }*/
+    
+     // basic apporach - flipping k from every 0
+    // gives TLE for last 2 big inputs
+     /*  public int minKBitFlips(int[] nums, int k) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i < nums.length - k + 1){
+                if(nums[i] == 0){
+                    flip(nums, i, k);
+                    count++;
+                }
+            }else{
+                if(nums[i] == 0) return -1;
+            }
+        }
+        return count;
+    }
+    
+    private void flip(int[] nums, int i, int k){
+        for(int j = i; j < i+k; j++){
+            nums[j] = 1-nums[j];
+        }    
+    } */
+}
