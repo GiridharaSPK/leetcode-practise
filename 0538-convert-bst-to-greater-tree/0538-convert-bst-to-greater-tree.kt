61d9d32efdb69d33abd27c099d76696ea60a223f
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-26 01:54:14 -0400

    Time: 194 ms (92.31%), Space: 38 MB (46.15%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.kt
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.kt
@@ -12,8 +12,48 @@ class Solution {
     var sum = 0
 
     fun convertBST(root: TreeNode?): TreeNode? {
-        // iterative apporach with stack
-        val stack = Stack<TreeNode>()
+        // Approach with Morris Traversal : constant space and O(n) time
+        // (no class variable usage)
+        // update links to avoid stack usage
+        // update links of right childs to parent to just keep on adding and moving 
+        // [opposite of inorder traversal example]
+        // but here we have to return the same tree 
+        // so we have to remove temp links without removing any original links
+        var node = root
+        var greaterSum = 0
+        while(node!=null){
+            if(node.right!=null){
+                // alter links
+                // of its left most child to current node
+                var r = node.right
+                while(r.left!=null && r.left!= node){
+                    r = r.left
+                }
+                // if here for the first time
+                if(r.left == null){ 
+                    // attaching to curr node - temporarily
+                    r.left = node 
+                    // move further right for next node   
+                    node = node.right                  
+                }else{ // already connected to parent node
+                    // visiting second time i.e after adding the sum of the right
+                    r.left = null // remove temp link
+                    greaterSum += node.`val` // add greaterSum to current node
+                    node.`val` = greaterSum // assign current greaterSum to current node
+                    // move left (already added right of current node)
+                    node = node.left
+                }
+            }else{ // no right node 
+                greaterSum += node.`val`
+                node.`val` = greaterSum
+                node = node.left
+            }
+        }
+        
+        return root
+        
+        // iterative apporach with stack : O(n) space and O(n) time
+        /*val stack = Stack<TreeNode>()
         var node = root
         while(node!=null || !stack.isEmpty()){
             // check right and add to stack
@@ -31,7 +71,7 @@ class Solution {
             // (if current has left, go left, else pop from stack again)
             node = temp.left
         }
-        return root
+        return root*/
         
         // recursive approach : O(n) space and O(n) time
         /*helper(root)
