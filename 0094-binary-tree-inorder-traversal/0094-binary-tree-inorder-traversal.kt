ce3ce26ef880edb7b680fb18caa1612ac8db6c60
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-26 00:25:01 -0400

    Time: 136 ms (91.59%), Space: 34 MB (81.07%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.kt
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.kt
@@ -10,9 +10,36 @@
  */
 class Solution {
     fun inorderTraversal(root: TreeNode?): List<Int> {
+        // morris traversal : O(n) time and constant space
+        // alter the links of left node's right ptr to point to head
+        var node = root
+        var temp = root
         val list = ArrayList<Int>()
-        helper(root, list)
+        
+        // at every node from root, check if there is a left node - alter links
+        // if no left node add to answer and move right
+        while(node!=null){ 
+            var left = node.left
+            if(left!=null){ // alter right most child/grandchild of the left 
+                while(left!!.right!=null){
+                    left = left.right
+                }
+                left!!.right = node
+                temp = node
+                node = node.left // assigning it to current `node`
+                temp.left = null
+            }else{ // no left node
+                list.add(node.`val`) // add to answer list
+                node = node.right // move right 
+            }
+        }
+        
         return list
+        
+        // recursion : O(n) space and O(n) time
+        /*val list = ArrayList<Int>()
+        helper(root, list)
+        return list*/
     }
     
     private fun helper(root: TreeNode?, list: ArrayList<Int>){
