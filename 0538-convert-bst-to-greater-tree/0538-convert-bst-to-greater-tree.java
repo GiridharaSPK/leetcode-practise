265c56cab509f54cee26dd5abdec910b9eda9784
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-26 02:13:21 -0400

    Time: 2 ms (11.69%), Space: 45.1 MB (68.79%) - LeetHub

---
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        // Morris Traversal Approach : O(n) time + constant space
+        
+        
+        // Iterative approach : O(n) time + O(n) space (stack)
+        // needs a class variable to maintain curr sum
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode node = root;
+        while(node!=null || !stack.isEmpty()){
+            // push all right nodes to stack
+            // right
+            while(node!=null){
+                stack.push(node);
+                node = node.right;
+            }
+            // current 
+            TreeNode temp = stack.pop();
+            sum += temp.val;
+            temp.val = sum;
+            
+            // go left
+            node = temp.left;
+        }
+        return root;
+        
+        // recursive apporach : O(n) time + O(n) space (recursive stack)
+        // needs a class variable for sum
+        /*helper(root);
+        return root;*/
+    }
+    
+    /*private void helper(TreeNode root){
+        if(root == null)
+            return;
+        
+        helper(root.right);
+        sum += root.val;
+        root.val = sum;
+        helper(root.left);
+    }*/
+    
+}
