51b7a48bd22e052327e33d7e1f9fe802c4dd3272
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-06-26 00:52:55 -0400

    Time: 0 ms (100.00%), Space: 41.2 MB (92.93%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,8 +15,36 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        //iterative approach
+        
+        // Morris traversal: O(n) time and constant space
+        // alter the links of left childs such that we just need to traverse right every time
+        // so finally we have no left childs,
+        // keep adding current node to answer list whenever there is no left node
         ArrayList<Integer> list = new ArrayList();
+        TreeNode node = root;
+        TreeNode temp = root;
+        while(node!=null){
+            // if current node has left child
+            // alter link of its right most child and point it to current node
+            if(node.left!=null){
+                TreeNode left = node.left;
+                while(left.right!=null){
+                    left = left.right;
+                }
+                // assign right of least child to current node (inorder traversal)
+                left.right = node;
+                temp = node;
+                node = node.left; // move leftwards for inorder traversal
+                temp.left = null; //mark left of current node to null (to avoid loop)
+            }else{ // no left node 
+                list.add(node.val); // add current node to answer
+                node = node.right; // move right
+            }
+        }
+        return list;
+        
+        //iterative approach
+        /*ArrayList<Integer> list = new ArrayList();
         Stack<TreeNode> s = new Stack<TreeNode>();
         TreeNode node = root;
         while(!s.isEmpty() || node!=null){
@@ -32,7 +60,8 @@ class Solution {
             //right
             node = node.right;
         }
-        return list;
+        return list;*/
+        
         // recursive approach
         // return helper(root, new ArrayList<Integer>());
     }
