c09c4a74531fe8d541cb8a2be6e809f38d6b5e04
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-07-16 12:58:18 -0400

    Time: 0 ms (100.00%), Space: 44.2 MB (24.00%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        
+        if(root.left!=null && removeLeafNodes(root.left, target) == null){
+            root.left = null;
+        }
+        if(root.right!=null && removeLeafNodes(root.right, target) == null){
+            root.right = null;
+        }
+        if(root.left == null && root.right == null && root.val == target){
+            return null;
+        }
+        return root;
+    }
+}
