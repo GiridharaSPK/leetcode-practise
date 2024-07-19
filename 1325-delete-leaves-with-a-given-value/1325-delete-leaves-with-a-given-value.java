72a4452169c2287b3418fbbab57f0e2c4aec2dcd
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-07-19 11:28:16 -0400

    Time: 0 ms (100.00%), Space: 43.9 MB (71.02%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
-        
+        if(root == null) return root;
         if(root.left!=null && removeLeafNodes(root.left, target) == null){
             root.left = null;
         }
@@ -25,6 +25,7 @@ class Solution {
         if(root.left == null && root.right == null && root.val == target){
             return null;
         }
+        
         return root;
     }
 }
