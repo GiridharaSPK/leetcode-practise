cf56e7e2acea37156c9a53efea6991965deb49f2
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-07-28 20:10:07 -0400

    Time: 2 ms (69.66%), Space: 42.2 MB (52.78%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
@@ -1,79 +1,66 @@
 class Solution {
     public int[] numsSameConsecDiff(int n, int k) {
-        // dfs
-        /*HashSet<Integer> set = new HashSet<>();
-
-        for(int d = 1; d < 10; d++) { // adding from 0 may cause stackoverflow
-            dfs(n-1, k, d, set);
+        HashSet<Integer> set = new HashSet<Integer>();
+        // BFS
+        // Queue<Integer> q = new LinkedList<Integer>();
+        // for(int i = 1; i < 10; i++){
+        //     q.offer(i);
+        // }
+        // bfs(n, k, q);
+        // int[] ans = new int[q.size()];
+        // int i = 0;
+        // while(!q.isEmpty()){
+        //     ans[i] = q.poll();
+        //     i++;
+        // }
+        // return ans;
+        
+        // DFS
+        for(int i = 1; i < 10; i++){
+            dfs(n-1, k, i, set);
         }
-
         int[] ans = new int[set.size()];
-        int j=0;
-        for(Integer i : set){
-            ans[j] = i;
-            j++;
-        }
-        return ans;*/
-        
-        
-        //bfs
-        Queue<Integer> q = new LinkedList<Integer>();
-        
-        for(int d=1;d<10;d++){
-            //adding from 0 may cause stackoverflow, anyway we dont start with 0
-            q.add(d); 
-        }
-        bfs(n, k, q);
-        int[] ans = new int[q.size()];
-        int j = 0;
-        for(Integer i : q){
-            ans[j] = i;
-            j++;
+        int i = 0;
+        for(int num: set){
+            ans[i] = num;
+            i++;
         }
         return ans;
     }
     
     private void bfs(int n, int k, Queue<Integer> q){
-        int level = 1;
         int size = q.size();
+        int level = 1;
         while(!q.isEmpty() && level < n){
-            level++;
-            size = q.size();
-            for(int i = 0; i < size; i++){
-                int num = q.remove();
-                int d = num%10;
-                if(d + k <= 9) 
-                    q.add(num*10 + d+k);
-                if(k!=0 && d - k >= 0) 
-                    q.add(num*10 + d-k);
+            if(size > 0){
+                int num = q.poll();
+                size--;
+                int last = num%10;
+                if(last + k < 10){
+                    q.offer(num*10 + last+k);
+                }
+                if(last - k >= 0){
+                    q.offer(num*10 + last-k);
+                }
+            }else{
+                size = q.size();
+                level++;
             }
         }
+        
     }
-    
-    /*private void bfs(int n, int k, Queue<Integer> q){
-        while(!q.isEmpty()){
-            int num = q.peek();
-                if(num >= Math.pow(10, n-1) && num < Math.pow(10,n)) return;
-            q.remove();
-            
-            int d = num%10;
-            if(d + k <= 9) 
-                q.add(num*10 + d+k);
-            if(k!=0 && d - k >= 0) 
-                q.add(num*10 + d-k);
-        }
-    }*/
-    
+        
     private void dfs(int n, int k, int num, HashSet<Integer> set){
-        if(n == 0) {
+        if(n == 0){
             set.add(num);
             return;
         }
-        int d = num % 10;
-        if(d + k <= 9) 
-            dfs(n-1, k, num*10+(d+k), set); // adding 0 to init list could cause stackoverflow
-        // as d+k and further nums keep becoming 0 
-        if(k!=0 && d - k >= 0) dfs(n-1, k, num*10+(d-k), set);
+        int last = num%10;
+        if(last+k < 10){
+            dfs(n-1, k, num*10 + (last+k), set);
+        }
+        if(last - k >= 0){
+            dfs(n-1, k, num*10 + (last-k), set);
+        }
     }
-    
 }
