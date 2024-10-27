aa6cc607767963b01e130ed237d3f353c5972b4d
Giridhara Sai Pavan Kumar Gurram <giridharaspk.gurram@gmail.com>
2024-10-27 18:13:22 -0400

    Time: 0 ms (100%), Space: 41.1 MB (85.66%) - LeetHub

--- 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.java
@@ -15,42 +15,38 @@
  */
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
-        if(root.val == x || root.val == y) return false;
+        if(root == null || root.val == x || root.val == y) return false;
 
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
+        int depthX = -1;
+        int depthY = -2;
+        int level = 0;
         while(!queue.isEmpty()){
-            int n = queue.size();
-            boolean found = false;
-            while(n > 0){
+            int size = queue.size();
+            while(size > 0){
                 TreeNode curr = queue.poll();
-                n--;    
-                boolean justFound = false;
-                if(curr.left!=null){ 
-                    queue.offer(curr.left);
-                    if(curr.left.val == x || curr.left.val == y){
-                        if(found){
-                            return true;
-                        }
-                        justFound = true;
-                        found = true;
-                    }
+                size--;    
+                if(curr.left!=null && curr.right!=null){
+                    if(curr.left.val == x && curr.right.val == y) return false;
+                    if(curr.left.val == y && curr.right.val == x) return false;
                 }
-                if(curr.right!=null){
+                if(curr.val == x){
+                    depthX = level;
+                }
+                if(curr.val == y){
+                    depthY = level;
+                }
+                if(curr.left != null){
+                    queue.offer(curr.left);
+                }
+                if(curr.right != null){
                     queue.offer(curr.right);
-                    if (curr.right.val == x || curr.right.val == y){
-                        if(justFound){
-                            return false;
-                        }else{
-                            if(found) return true;
-                        }
-                        found = true;
-                    }
                 }
             }
+            level++;
         }
-
-        return false;
+        return depthX == depthY;
 
     }
 }
